d1e93ea076c8e971bb24bd449f459d68df7af403
vagrant <datsitsre>
2023-05-12 10:59:32 +0000

    Dict method

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from uuid import uuid4
 from datetime import datetime
-
+import models
 
 class BaseModel:
     """
@@ -15,15 +15,10 @@ class BaseModel:
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
-    def __str__(self):
-        """
-            Print some information 
-        """
-        return "[BaseModel] ({}) {}".format(self.id, self.__dict__)
 
     def save(self):
         """
-            Save method
+            Save method update the updated_at attributes
         """
         self.updated_at = datetime.now()
 
@@ -31,5 +26,17 @@ class BaseModel:
         """
             Print  show the dict of the object 
         """
-        return "{}".format(self.__dict__)
+        base_dic = {}
+        base_dic = self.__dict__.copy()
+        if "created_at" in base_dic:
+            base_dic["created_at"]  = self.created_at.isoformat()
+        if "updated_at" in base_dic:
+            base_dic["updated_at"]  = self.updated_at.isoformat()
+        base_dic["__class__"] = self.__class__.__name__
+        return base_dic
         
+    def __str__(self):
+        """
+            Print some information 
+        """
+        return "[{}] ({}) {}".format(self.__class__.__name__,self.id, self.__dict__)
