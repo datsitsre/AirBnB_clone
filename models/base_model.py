2ae5b913a0c3cbb1ee0a26c1c851681632277e02
Sir-Blow <emmanuelakej@gmail.com>
2023-05-16 04:09:48 -0700

    Base_Model

--- models/base_model.py
+++ models/base_model.py
@@ -1,55 +1,47 @@
 #!/usr/bin/python3
+"""module of 'BaseModel' class"""
+
 from uuid import uuid4
 from datetime import datetime
 import models
-import json
 
 
 class BaseModel:
-    """
-        Base Model main
-    """
-    def __init__(self, *args, **kwargs):
-        """
-            The public instance
-        """
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    """Representation of a BaseModel"""
 
-        time_format = "%Y-%m-%dT%H:%M:%S.%f"
-        if kwargs:
+    def __init__(self, *args, **kwargs):
+        """class constructor"""
+        if len(kwargs) > 0:
             for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    self.__dict__[key] = datetime.strptime(value, time_format)
-                else:
-                    self.__dict__[key] = value
+                if key in ["created_at", "updated_at"]:
+                    setattr(self, key,
+                            datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                elif key != "__class__":
+                    setattr(self, key, value)
+
         else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
             models.storage.new(self)
+            models.storage.save()
+
+    def __str__(self):
+        """custom __str__ method for BaseModel"""
+        return ("[{}] ({}) {}".format(self.__class__.__name__,
+                                      self.id, self.__dict__))
 
     def save(self):
-        """
-            Save method update the updated_at attributes
-        """
+        """updates updated_at with the current datetime"""
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        """
-            Print  show the dict of the object
-        """
-        base_dic = {}
-        base_dic = self.__dict__.copy()
-        if "created_at" in base_dic:
-            base_dic["created_at"] = self.created_at.isoformat()
-        if "updated_at" in base_dic:
-            base_dic["updated_at"] = self.updated_at.isoformat()
-        base_dic["__class__"] = self.__class__.__name__
-        return base_dic
+        """returns a dictionary containing all keys/values
+           of __dict__ of the instance"""
 
-    def __str__(self):
-        """
-            Print some information
-        """
-        return "[{}] ({}) {}".format(self.__class__.__name__,
-                                     self.id, self.__dict__)
+        dict_ = dict(self.__dict__)
+        dict_.update({"__class__": self.__class__.__name__,
+                      "created_at": str(((self.created_at).isoformat())),
+                      "updated_at": str(((self.updated_at).isoformat()))})
+        return dict_
